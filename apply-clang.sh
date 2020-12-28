388db4bb8751f23b6b013cc544637282ffe53d3b
Damian Philipp <damian.philipp@gmx.net>
2020-12-28 13:05:19 +0100

    Added clang-format configuration and script

---
+++ apply-clang.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+CLANG_BINARY="/c/Program Files/LLVM/bin/clang-format.exe"
+
+for filename in `find src include -iname "*.cpp" -o -iname "*.h"`
+do
+    echo "Formatting $filename"
+    "$CLANG_BINARY" -i "$filename"
+done
+
+for filename in `find test -name "argtable3" -prune -o -iname "*.cpp" -o -iname "*.h"`
+do
+    echo "Formatting $filename"
+    "$CLANG_BINARY" -i "$filename"
+done
